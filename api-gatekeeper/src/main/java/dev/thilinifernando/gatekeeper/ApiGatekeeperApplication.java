4b59f293223483abb102d3f504ca01ad6efb573b
Thilini Fernando <fthilini34@gmail.com>
2026-03-14 18:41:35 +0530

    initiate the api gateway

---
+++ api-gatekeeper/src/main/java/dev/thilinifernando/gatekeeper/ApiGatekeeperApplication.java
@@ -0,0 +1,13 @@
+package dev.thilinifernando.gatekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatekeeperApplication.class, args);
+	}
+
+}
---
+++ api-gatekeeper/src/test/java/dev/thilinifernando/gatekeeper/ApiGatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thilinifernando.gatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
