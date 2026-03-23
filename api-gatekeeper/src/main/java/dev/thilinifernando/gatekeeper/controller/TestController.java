27a7da1a37cc85dceccd4d7ab59f1ccbdaa9ae75
Thilini Fernando <fthilini34@gmail.com>
2026-03-23 07:22:45 +0530

    config rate limiting through creating an object

--- api-gatekeeper/src/main/java/dev/thilinifernando/gatekeeper/controller/TestController.java
+++ api-gatekeeper/src/main/java/dev/thilinifernando/gatekeeper/controller/TestController.java
@@ -1,4 +1,15 @@
 package dev.thilinifernando.gatekeeper.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "test")
 public class TestController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
