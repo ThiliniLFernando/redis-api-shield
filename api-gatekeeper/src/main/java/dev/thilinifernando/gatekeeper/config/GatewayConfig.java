979472c6d537094b64c9f9b3ea24a0aa363a2c57
Thilini Fernando <fthilini34@gmail.com>
2026-04-12 16:56:04 +0530

    create new gateway config class

---
+++ api-gatekeeper/src/main/java/dev/thilinifernando/gatekeeper/config/GatewayConfig.java
@@ -0,0 +1,22 @@
+package dev.thilinifernando.gatekeeper.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service-route", r -> r
+                        .path("/users/**")
+                        .filters(f -> f
+                                .stripPrefix(1)
+                                .prefixPath("/api/v1/users")
+                        )
+                        .uri("http://localhost:8082")).build();
+    }
+}
